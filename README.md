f28b2ef3f4ab67ef4e01ae19f4e5aabbc159b81f
WiSmKo <william@thewhisk.dev>
2025-07-26 11:53:14 +0200

    registerAvailability tests improved, update Junit and start Readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ride Matching Service Readme
+
+## Tests
+
+A suite of unit tests for the service can be found in `test/java/services/RideMatchingServiceTest` which tests the main requirements of the application.
+
+## Setup and Execution
+
+For the sake of the scope of this exercise, the service can be triggered through the main class, with data stored in memory as per the assignment specification.
+In a real production environment additional layers would be implemented, there would be a database for persisting the drivers and rides and a controller layer to
+facilitate use of the service from external sources such as through an app or browser.
+
+
--- src/test/java/services/RideMatchingServiceTest.java
+++ src/test/java/services/RideMatchingServiceTest.java
@@ -1,42 +1,58 @@
 package services;
 
-import org.junit.Test;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.wismko.models.Driver;
 import org.wismko.models.Location;
 import org.wismko.services.RideMatchingService;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class RideMatchingServiceTest {
 
 
     private RideMatchingService rideMatchingService;
 
+    @BeforeEach
+    void setUp(){
+        this.rideMatchingService = new RideMatchingService();
 
+        rideMatchingService.drivers = List.of(
+            new Driver( "driver1", true, new Location( 1.0, 2.0 ) ),
+            new Driver(  "driver2", false, new Location( 3.0, 4.0 ))
+        );
+    }
 
-    @Test
-    public void registerDriverAvailability_available(){
-        String driverId = "123";
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void registerDriverAvailability_newDriver( boolean available ){
+        String driverId = "newDriver";
         Location location= new Location( 45.34, 67.56 );
 
-        rideMatchingService.registerDriverAvailability(driverId, true, location);
+        rideMatchingService.registerDriverAvailability(driverId, available, location);
 
         Driver expectedDriver = rideMatchingService.drivers.stream().filter( driver -> driver.getId().equals( driverId ) ).findFirst().get();
-        assertTrue( expectedDriver.isAvailable() );
+        assertEquals( expectedDriver.isAvailable(), available );
         assertEquals( expectedDriver.getLocation(), location );
     }
 
-    @Test
-    public void registerDriverAvailability_notAvailable(){
-        String driverId = "123";
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void registerDriverAvailability_existingDriver( boolean available ){
+        String driverId = "driver1";
         Location location= new Location( 45.34, 67.56 );
 
-        rideMatchingService.registerDriverAvailability(driverId, true, location);
+        rideMatchingService.registerDriverAvailability(driverId, available, location);
 
         Driver expectedDriver = rideMatchingService.drivers.stream().filter( driver -> driver.getId().equals( driverId ) ).findFirst().get();
-        assertFalse( expectedDriver.isAvailable() );
+        assertEquals( expectedDriver.isAvailable(), available );
+        assertEquals( expectedDriver.getLocation(), location );
     }
 
     @Test
